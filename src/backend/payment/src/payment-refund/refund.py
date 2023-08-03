634ec126cc63d21ae59b17e3275846d67a36fa1d
Fernando Gamero <gamerf@amazon.com>
2023-08-03 16:55:27 +0100

    replacing stripe as payment

---
+++ src/backend/payment/src/payment-refund/refund.py
@@ -0,0 +1,17 @@
+import os
+import json
+
+from aws_lambda_powertools import Logger, Metrics
+
+logger = Logger()
+metrics = Metrics()
+
+@metrics.log_metrics(capture_cold_start_metric=True)
+def lambda_handler(event, context):
+    logger.info(event)
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Payment refund processed!')
+    
+    }
