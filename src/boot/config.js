5c1d7e48bc315603686dacbae4d02fc3facd86e1
gamerf <50994049+gamerf@users.noreply.github.com>
2023-11-01 16:43:22 +0000

    Update config.js

--- src/boot/config.js
+++ src/boot/config.js
@@ -17,8 +17,8 @@ window.Config = {
   BOOKING_CREATE: 'https://endpckl3zl877.x.pipedream.net/',
 
   //Stripe public key
-  STRIPE_KEY: "pk_test_51NalZoDM0zU4TbBSZyTyeXASocV3YREIITEe7ywrHHDmGcNtPQWyLi4EBceaVoyQTQIAaBl4HuA8OOzn5gkXIxYm00vT1JuHnG",
+  STRIPE_KEY: "pk_test_",
 
   //Stripe secret key
-  STRIPE_SECRET_KEY: "sk_test_51NalZoDM0zU4TbBSnhasjNqHEV1f2hyipS8m7JYIZvgk8o0iA8Nb0GBe6w7yiSH2gfIP0kNuQ6LQgSk8JvFkZfmE00QhFXL8CZ"
+  STRIPE_SECRET_KEY: "sk_test_"
 }
