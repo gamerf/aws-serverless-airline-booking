8ad7278a8148ed1426338b13270503314d542aed
Fernando Gamero <gamerf@amazon.com>
2023-11-01 16:29:42 +0000

    Initial commit of workshop-fix

---
+++ src/boot/config.js
@@ -0,0 +1,24 @@
+window.Config = {
+  // List all flights
+  CATALOG_LIST: 'https://endpckl3zl877.x.pipedream.net/',
+  // Find a flight
+  CATALOG_FETCH: 'https://endpckl3zl877.x.pipedream.net/',
+
+  // Loyalty fetch
+  LOYALTY_FETCH: 'https://endpckl3zl877.x.pipedream.net/',
+
+  // Payment pre-auth
+  PAYMENT_PREAUTH: 'https://endpckl3zl877.x.pipedream.net/',
+
+  // Bookings fetch
+  BOOKING_FETCH: 'https://endpckl3zl877.x.pipedream.net/',
+
+  // Book create
+  BOOKING_CREATE: 'https://endpckl3zl877.x.pipedream.net/',
+
+  //Stripe public key
+  STRIPE_KEY: "pk_test_51NalZoDM0zU4TbBSZyTyeXASocV3YREIITEe7ywrHHDmGcNtPQWyLi4EBceaVoyQTQIAaBl4HuA8OOzn5gkXIxYm00vT1JuHnG",
+
+  //Stripe secret key
+  STRIPE_SECRET_KEY: "sk_test_51NalZoDM0zU4TbBSnhasjNqHEV1f2hyipS8m7JYIZvgk8o0iA8Nb0GBe6w7yiSH2gfIP0kNuQ6LQgSk8JvFkZfmE00QhFXL8CZ"
+}
---
+++ src/store/bookings/payment.js
@@ -0,0 +1,76 @@
+import Flight from '../../shared/models/FlightClass' // eslint-disable-line
+import { Loading } from 'quasar'
+import axios from 'axios'
+
+const paymentEndpoint =
+  process.env.VUE_APP_PaymentChargeUrl || 'no payment gateway endpoint set'
+
+/**
+ *
+ * Process Payment function - processPayment calls Payment endpoint to pre-authorize charge upon tokenized payment details
+ *
+ * @param {object} obj - Object containing params to process payment
+ * @param {object} obj.paymentToken - Tokenized payment info
+ * @param {object} obj.paymentToken.details - Tokenized payment details including last4, id, etc.
+ * @param {object} obj.paymentToken.id - Payment token
+ * @param {Flight} obj.outboundFlight - Outbound flight
+ * @param {string} obj.customerEmail - Customer Email address for payment notification
+ * @returns {promise} - Promise representing whether payment was successfully pre-authorized
+ * @example
+ *   let chargeToken = await processPayment({
+ *      paymentToken,
+ *      outboundFlight,
+ *      customerEmail
+ *   });
+ */
+export async function processPayment({
+  paymentToken,
+  outboundFlight,
+  customerEmail,
+  idToken
+}) {
+  console.group('store/bookings/actions/processPayment')
+  Loading.show({
+    message: 'Processing payment...'
+  })
+
+  if (!paymentToken) throw 'Invalid payment token'
+
+  const chargeData = {
+    amount: outboundFlight.ticketPrice,
+    currency: outboundFlight.ticketCurrency,
+    stripeToken: paymentToken.details.id,
+    description: `Payment by ${customerEmail}`,
+    email: customerEmail
+  }
+
+  console.log('Charge data to be processed')
+  console.log(chargeData)
+  try {
+    const {
+      data: {
+        createdCharge: { id: chargeId }
+      }
+    } = await axios.put(window.Config.PAYMENT_PREAUTH,
+      {
+        data: chargeData,
+      },
+      {
+        headers: {
+        Authorization: idToken,
+        'Content-Type': 'application/json'
+        }
+      }
+    )
+
+    Loading.show({
+      message: 'Payment authorized successfully...'
+    })
+
+    console.groupEnd()
+    return chargeId
+  } catch (err) {
+    console.error(err)
+    throw err
+  }
+}
